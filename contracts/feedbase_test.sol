8cf5f666889f75744a24604965d49d29a760efdb
Daniel Brockman <daniel@brockman.se>
2016-05-18 17:55:05 +0200

    Clean up test code

--- contracts/feedbase_test.sol
+++ contracts/feedbase_test.sol
@@ -2,101 +2,114 @@ import "dapple/test.sol";
 import "dappsys/token/base.sol";
 import "feedbase.sol";
 
-contract GetProxy is Tester {
-    function doGet(uint64 id) returns (bytes32 value) {
+contract FeedbaseTester is Tester {
+    function get(uint64 id) returns (bytes32 value) {
         return Feedbase(_t).get(id);
     }
 }
-contract FeedbaseTest is Test {
-    Feedbase fb;
-    GetProxy t1;
-    uint64 feed1;
-    DSToken dai = new DSTokenBase(1000);
-    function setUp() {
-        fb = new Feedbase();
-        t1 = new GetProxy();
-        t1._target(fb);
-        feed1 = fb.claim();
-    }
-    function testSetup() {
-        assertEq(uint(0), uint(feed1));
-    }
-    function testUpdateGetFree() {
-        fb.update(feed1, 0x42, block.timestamp+1);
-        assertEq32(t1.doGet(feed1), 0x42);
-    }
-    function testUpdateGetPaid() {
-        fb.update(feed1, 0x42, block.timestamp+1);
-        fb.setFee(feed1, 100, dai);
-        dai.transfer(t1, 100);
 
-        t1._target(dai);
-        DSToken(t1).approve(fb, 100);
-        
-        t1._target(fb);
-        assertEq(dai.balanceOf(t1), 100);
-        var pre = dai.balanceOf(this);
-        var value = t1.doGet(feed1);
-        var post = dai.balanceOf(this);
-        assertEq(post - pre, 100);
-        assertEq(dai.balanceOf(t1), 0);
+contract FeedbaseTest is Test {
+    event Update(uint64 indexed id);
+    
+    Feedbase feedbase = new Feedbase();
+    DSToken dai = new DSTokenBase(1000);
+    FeedbaseTester tester = new FeedbaseTester();
+
+    uint64 id;
+
+    function setUp() {
+        tester._target(feedbase);
+        id = feedbase.claim();
+    }
+
+    function test_claim() {
+        assertEq(uint(0), uint(id));
+        assertEq(uint(1), feedbase.claim());
+        assertEq(uint(2), feedbase.claim());
+    }
+
+    function test_get_free_feed() {
+        feedbase.update(id, 0x42, block.timestamp + 1);
+        assertEq32(tester.get(id), 0x42);
+    }
+
+    function test_get_paid_feed() {
+        feedbase.update(id, 0x42, block.timestamp + 1);
+        feedbase.setFee(id, 100, dai);
+        dai.transfer(tester, 100);
+
+        tester._target(dai);
+        ERC20(tester).approve(feedbase, 100);
+
+        tester._target(feedbase);
+        assertEq(dai.balanceOf(tester), 100);
+        var initial = dai.balanceOf(this);
+        var value = tester.get(id);
+        assertEq(dai.balanceOf(this) - initial, 100);
+        assertEq(dai.balanceOf(tester), 0);
         assertEq32(value, 0x42);
     }
-    function testFailUpdateGetPaid() {
-        fb.update(feed1, 0x42, block.timestamp+1);
-        fb.setFee(feed1, 100, dai);
-        dai.transfer(t1, 99);
+    
+    function testFail_get_paid_feed() {
+        feedbase.update(id, 0x42, block.timestamp + 1);
+        feedbase.setFee(id, 100, dai);
+        dai.transfer(tester, 99);
 
-        t1._target(dai);
-        DSToken(t1).approve(fb, 100);
+        tester._target(dai);
+        DSToken(tester).approve(feedbase, 100);
 
-        t1._target(fb);
-        t1.doGet(feed1);
+        tester._target(feedbase);
+        tester.get(id);
     }
-    function testUpdateGetTwicePaidOnce() {
-        fb.update(feed1, 0x42, block.timestamp+1);
-        fb.setFee(feed1, 100, dai);
-        dai.transfer(t1, 100);
-        
-        t1._target(dai);
-        DSToken(t1).approve(fb, 100);
-        
-        t1._target(fb);
+    
+    function test_get_paid_feed_twice() {
+        feedbase.update(id, 0x42, block.timestamp + 1);
+        feedbase.setFee(id, 100, dai);
+        dai.transfer(tester, 100);
+
+        tester._target(dai);
+        ERC20(tester).approve(feedbase, 100);
+
+        tester._target(feedbase);
         var pre = dai.balanceOf(this);
-        var value1 = t1.doGet(feed1);
+        var value1 = tester.get(id);
         var post1 = dai.balanceOf(this);
-        var value2 = t1.doGet(feed1);
+        var value2 = tester.get(id);
         var post2 = dai.balanceOf(this);
         assertEq(post1 - pre, 100);
         assertEq(post2 - post1, 0);
         assertEq32(value1, 0x42);
         assertEq32(value2, 0x42);
     }
-    function testUpdateGetPaidTokenBogus() {
-        // A bogus token cost behaves as a zero cost
-        fb.update(feed1, 0x42, block.timestamp+1);
-        fb.setFee(feed1, 100, ERC20(0xdeadbeef));
-        assertEq32(t1.doGet(feed1), 0x42);
+    
+    function test_get_feed_with_bogus_fee_token() {
+        feedbase.update(id, 0x42, block.timestamp + 1);
+        feedbase.setFee(id, 100, ERC20(0xdeadbeef));
+        assertEq32(tester.get(id), 0x42);
     }
-    function testFailGetExpiredFeed() {
-        fb.update(feed1, 0x42, block.timestamp-1);
-        fb.get(feed1);
+    
+    function testFail_get_expired_feed() {
+        feedbase.update(id, 0x42, block.timestamp - 1);
+        feedbase.get(id);
     }
-    function testTransfer() {
-        fb.transfer(feed1, t1);
-        Feedbase(t1).update(feed1, 0x123, block.timestamp+1);
-        assertEq32(fb.get(feed1), 0x123);
+    
+    function test_transfer() {
+        feedbase.transfer(id, tester);
+        Feedbase(tester).update(id, 0x123, block.timestamp+1);
+        assertEq32(feedbase.get(id), 0x123);
     }
-    event Update( uint64 indexed id );
-    function testEvents() {
-        expectEventsExact(fb);
-        fb.setFee(feed1, 0, dai);
-        Update(feed1);
-        fb.update(feed1, 0x42, block.timestamp+1);
-        Update(feed1);
-        fb.transfer(feed1, t1);
-        Update(feed1);
-        var feed2 = fb.claim();
-        Update(feed2);
+    
+    function test_events() {
+        expectEventsExact(feedbase);
+        feedbase.setFee(id, 0, dai);
+        Update(id);
+        feedbase.setDescription(id, "foo");
+        Update(id);
+        feedbase.update(id, 0x42, block.timestamp + 1);
+        Update(id);
+        feedbase.transfer(id, tester);
+        Update(id);
+        var id2 = feedbase.claim();
+        Update(id2);
     }
 }
