3986a54b6e8e999f51669ee822ca54fa0cb69c88
Daniel Brockman <daniel@brockman.se>
2016-07-21 08:44:21 +0200

    huge crazy refactoring, a big improvement I think
    Should have done this before Peter started looking into this code,
    but oh well... the biggest changes were necessary anyway.
    
    (fixes #12, #13)

--- contracts/feedbase.sol
+++ contracts/feedbase.sol
@@ -1,143 +1,188 @@
-import "dappsys/token/erc20.sol";
+/// feedbase.sol --- feedbase is to feeds as a database is to data
 
-contract FeedbaseEvents {
-    event Claimed    (uint72 indexed id);
-    event Configured (uint72 indexed id);
-    event Published  (uint72 indexed id);
-    event Paid       (uint72 indexed id);
+import "erc20/erc20.sol";
+
+// The reason why we use `uint72' for feed IDs is to help prevent
+// accidentally confusing different values that have the same type.
+//
+// In particular, this is very error-prone when dealing with functions
+// that take long lists of different parameters or return many values.
+//
+// Since `uint72' is a very uncommon type, it's much less likely that
+// someone would mix up a feed ID with something completely unrelated.
+
+contract FeedbaseEvents
+{
+    // Events logged by producers
+    event Claimed      (uint72 indexed id,  address owner,  ERC20 token);
+    event Published    (uint72 indexed id,  uint40  expiration);
+    event Repriced     (uint72 indexed id,  uint    price);
+    event Redescribed  (uint72 indexed id,  bytes32 description);
+    event Transferred  (uint72 indexed id,  address recipient);
+
+    // Events logged by consumers
+    event Purchased    (uint72 indexed id,  address consumer);
 }
 
-contract Feedbase is FeedbaseEvents {
-    Feed[2**72]  feeds;
-    uint72       next;
+contract Feedbase is FeedbaseEvents
+{
+    Feed[2**72]    feeds;
+    uint72         next;
 
     struct Feed {
-        address  owner;
-        bytes32  description;
-        uint     fee;
-        ERC20    token;
+        address    owner;
+        ERC20      token;
 
-        bytes32  value;
-        uint40   timestamp;
-        uint40   expiration;
-        bool     paid;
+        bytes32    description;
+        uint       price;
+
+        bytes32    value;
+        uint40     timestamp;
+        uint40     expiration;
+
+        bool       purchased;
     }
+
+    //------------------------------------------------------------------
+    // Public accessor functions
+    //------------------------------------------------------------------
 
     function owner(uint72 id)
     constant returns (address) { return feeds[id].owner; }
+    function token(uint72 id)
+    constant returns (ERC20)   { return feeds[id].token; }
+    function charity(uint72 id)
+    constant returns (bool)    { return token(id) == ERC20(0); }
 
     function description(uint72 id)
     constant returns (bytes32) { return feeds[id].description; }
-
-    function fee(uint72 id)
-    constant returns (uint)    { return feeds[id].fee; }
-
-    function token(uint72 id)
-    constant returns (ERC20)   { return feeds[id].token; }
+    function price(uint72 id)
+    constant returns (uint)    { return feeds[id].price; }
 
     function timestamp(uint72 id)
     constant returns (uint40)  { return feeds[id].timestamp; }
-
     function expiration(uint72 id)
     constant returns (uint40)  { return feeds[id].expiration; }
+    function expired(uint72 id)
+    constant returns (bool)    { return now >= expiration(id); }
 
-    function paid(uint72 id)
-    constant returns (bool)    { return feeds[id].paid; }
-
-    //------------------------------------------------------------------
-
-    function expired(uint72 id) constant returns (bool) {
-        return block.timestamp > feeds[id].expiration;
-    }
-
-    function free(uint72 id) constant returns (bool) {
-        return address(feeds[id].token) == address(0);
-    }
-
-    function unpaid(uint72 id) constant returns (bool) {
-        return !free(id) && !feeds[id].paid;
-    }
+    function purchased(uint72 id)
+    constant returns (bool)    { return feeds[id].purchased; }
 
     //------------------------------------------------------------------
-    // For feed owners
+    // Feed producer entrypoints
     //------------------------------------------------------------------
+
+    function claim() returns (uint72 id) {
+        id = claim(ERC20(0));
+    }
 
     function claim(ERC20 token) returns (uint72 id) {
         id = next++;
-        if (next == 0) throw;
+        assert(next != 0);
+
         feeds[id].owner = msg.sender;
         feeds[id].token = token;
-        Claimed(id);
-    }
 
-    function claim() returns (uint72 id) {
-        return claim(ERC20(0));
+        Claimed(id, msg.sender, token);
     }
 
     modifier auth(uint72 id) {
-        if (msg.sender != feeds[id].owner) throw;
+        assert(msg.sender == owner(id));
         _
     }
 
-    function set_description(uint72 id, bytes32 description)
-        auth(id)
-    {
-        feeds[id].description = description;
-        Configured(id);
-    }
-
-    function set_fee(uint72 id, uint fee)
-        auth(id)
-    {
-        if (free(id)) throw;
-        feeds[id].fee = fee;
-        Configured(id);
-    }
+    //------------------------------------------------------------------
 
     function publish(uint72 id, bytes32 value, uint40 expiration)
         auth(id)
     {
         feeds[id].value       = value;
-        feeds[id].timestamp   = uint40(block.timestamp);
+        feeds[id].timestamp   = uint40(now);
         feeds[id].expiration  = expiration;
-        feeds[id].paid        = false;
-        Published(id);
+        feeds[id].purchased   = false;
+
+        Published(id, expiration);
     }
 
-    function transfer(uint72 id, address owner)
+    //------------------------------------------------------------------
+
+    function reprice(uint72 id, uint price)
         auth(id)
     {
-        feeds[id].owner = owner;
-        Configured(id);
+        assert(!charity(id));
+        feeds[id].price = price;
+        Repriced(id, price);
+    }
+
+    function redescribe(uint72 id, bytes32 description)
+        auth(id)
+    {
+        feeds[id].description = description;
+        Redescribed(id, description);
+    }
+
+    function transfer(uint72 id, address recipient)
+        auth(id)
+    {
+        feeds[id].owner = recipient;
+        Transferred(id, recipient);
     }
 
     //------------------------------------------------------------------
-    // For consumers
+    // The feed consumer entrypoint
     //------------------------------------------------------------------
 
-    function read(uint72 id) returns (bool ok, bytes32 value) {
-        if (!expired(id) && pay(id, msg.sender)) {
-            return (true, feeds[id].value);
+    function read(uint72 id) validate(id)
+        returns (bool ok, bytes32 value)
+    {
+        ok     = true;
+        value  = feeds[id].value;
+    }
+
+    //------------------------------------------------------------------
+
+    modifier validate(uint72 id) {
+        if (!expired(id) && eligible(msg.sender, id)) _
+    }
+
+    function eligible(address consumer, uint72 id)
+        internal returns (bool ok)
+    {
+        if (charity(id) || purchased(id)) {
+            ok = true;
         } else {
-            return (false, 0);
+            ok = buy(consumer, id);
         }
     }
 
-    function pay(uint72 id, address payer) internal returns (bool ok) {
-        if (!unpaid(id)) return true;
+    // This wrapper function around `purchase()' normalizes the
+    // prescence or abscence of errors into a single boolean value.
+    function buy(address consumer, uint72 id)
+        internal returns (bool ok)
+    {
+        ok = this.call(
+            bytes4(sha3("purchase(address,uint72)")),
+            consumer, id
+        );
+    }
 
-        var feed = feeds[id];
+    modifier loopback() {
+        assert(msg.sender == address(this));
+        _
+    }
 
-        feed.paid = true;
+    function purchase(address consumer, uint72 id)
+        loopback
+    {
+        feeds[id].purchased = true;
+        assert(token(id).transferFrom(consumer, owner(id), price(id)));
+        Purchased(id, consumer);
+    }
 
-        ok = feed.token.call(bytes4(sha3(
-            "transferFrom(address,address,uint256)"
-        )), payer, feed.owner, feed.fee);
+    //------------------------------------------------------------------
 
-        if (ok) {
-            Paid(id);
-        } else {
-            feed.paid = false;
-        }
+    function assert(bool predicate) internal {
+        if (!predicate) throw;
     }
 }
--- contracts/feedbase_test.sol
+++ contracts/feedbase_test.sol
@@ -1,133 +1,237 @@
 import "dapple/test.sol";
-import "dappsys/token/base.sol";
-
+import "erc20/erc20.sol";
 import "feedbase.sol";
 
-contract FeedbaseTester is Tester {
-    // Type declaration needed to retrieve return values
-    function read(uint72 id) returns (bool ok, bytes32 value) {
+contract FeedbaseTest is Test,
+    FeedbaseEvents
+{
+    FakePerson  assistant  = new FakePerson();
+    FakeToken   token      = new FakeToken();
+    Feedbase    feedbase   = new Feedbase();
+
+    uint72      id;
+
+    function setUp() {
+        assistant._target(feedbase);
+        id = feedbase.claim(token);
+    }
+
+    function soon()     returns (uint40) { return uint40(now + 1); }
+    function recently() returns (uint40) { return uint40(now - 1); }
+
+    //------------------------------------------------------------------
+    // Feed configuration and setup
+    //------------------------------------------------------------------
+
+    function test_claim() {
+        expectEventsExact(feedbase);
+
+        Claimed(feedbase.claim(), address(this), ERC20(0));
+        Claimed(feedbase.claim(token), address(this), token);
+    }
+
+    //------------------------------------------------------------------
+
+    function test_reprice() {
+        expectEventsExact(feedbase);
+
+        feedbase.reprice (id, 50);
+        Repriced         (id, 50);
+
+        assertEq(feedbase.price(id), 50);
+    }
+
+    function testFail_reprice_illicit() {
+        Feedbase(assistant).reprice(id, 50);
+    }
+
+    function testFail_reprice_charity() {
+        feedbase.reprice(feedbase.claim(), 50);
+    }
+
+    //------------------------------------------------------------------
+
+    function test_redescribe() {
+        expectEventsExact(feedbase);
+
+        feedbase.redescribe (id, "foo");
+        Redescribed         (id, "foo");
+
+        assertEq32(feedbase.description(id), "foo");
+    }
+
+    function testFail_redescribe_illicit() {
+        Feedbase(assistant).redescribe(id, "foo");
+    }
+
+    //------------------------------------------------------------------
+
+    function test_transfer() {
+        expectEventsExact(feedbase);
+
+        feedbase.transfer (id, assistant);
+        Transferred       (id, assistant);
+
+        Feedbase(assistant).reprice (id, 50);
+        Repriced                    (id, 50);
+
+        assertEq(feedbase.price(id), 50);
+    }
+
+    //------------------------------------------------------------------
+    // Publishing and reading values
+    //------------------------------------------------------------------
+
+    function test_read_free() {
+        expectEventsExact(feedbase);
+        
+        id = feedbase.claim();
+        Claimed          (id, address(this), ERC20(0));
+
+        feedbase.publish (id, 0xffffffff, soon());
+        Published        (id,             soon());
+
+        var (ok, value) = assistant.read(id);
+        assertTrue(ok);
+        assertEq32(value, 0xffffffff);
+    }
+
+    function test_read_priced() {
+        expectEventsExact(feedbase);
+
+        feedbase.reprice (id, 50);
+        Repriced         (id, 50);
+
+        feedbase.publish (id, 0xffffffff, soon());
+        Published        (id,             soon());
+
+        token.allocate(assistant, 2000);
+
+        var (ok, value) = assistant.read(id);
+        Purchased(id, address(assistant));
+        assertTrue(ok);
+        assertEq32(value, 0xffffffff);
+
+        assertEq(token.balances(assistant), 1950);
+    }
+
+    function test_read_priced_again() {
+        expectEventsExact(feedbase);
+
+        feedbase.reprice (id, 50);
+        Repriced         (id, 50);
+
+        feedbase.publish (id, 0xffffffff, soon());
+        Published        (id,             soon());
+
+        token.allocate(assistant, 2000);
+
+        var (ok_1, value_1) = assistant.read(id);
+        Purchased(id, address(assistant));
+        assertTrue(ok_1);
+        assertEq32(value_1, 0xffffffff);
+
+        var (ok_2, value_2) = assistant.read(id);
+        assertTrue(ok_2);
+        assertEq32(value_2, 0xffffffff);
+
+        assertEq(token.balances(assistant), 1950);
+    }
+
+    function test_read_priced_no_money() {
+        expectEventsExact(feedbase);
+
+        feedbase.reprice (id, 50);
+        Repriced         (id, 50);
+
+        feedbase.publish (id, 0xffffffff, soon());
+        Published        (id,             soon());
+
+        token.allocate(assistant, 49);
+
+        var (ok, value) = assistant.read(id);
+        assertFalse(ok);
+        assertEq32(value, 0x0);
+
+        assertEq(token.balances(assistant), 49);
+    }
+
+    function test_read_priced_no_money_no_throwing() {
+        expectEventsExact(feedbase);
+
+        feedbase.reprice (id, 50);
+        Repriced         (id, 50);
+
+        feedbase.publish (id, 0xffffffff, soon());
+        Published        (id,             soon());
+
+        token.allocate(assistant, 49);
+        token.disable_throwing();
+
+        var (ok, value) = assistant.read(id);
+        assertFalse(ok);
+        assertEq32(value, 0x0);
+
+        assertEq(token.balances(assistant), 49);
+    }
+
+    function test_read_expired() {
+        expectEventsExact(feedbase);
+
+        feedbase.publish (id, 0xffffffff, recently());
+        Published        (id,             recently());
+
+        var (ok, value) = feedbase.read(id);
+        assertFalse(ok);
+        assertEq32(value, 0x0);
+    }
+}
+
+//----------------------------------------------------------------------
+
+contract FakePerson is Tester
+{
+    // We need to have a custom implementation of this particular
+    // function in order to be able to access these return values.
+    function read(uint72 id) returns (bool, bytes32) {
         return Feedbase(_t).read(id);
     }
 }
 
-contract FeedbaseTest is Test, FeedbaseEvents {
-    DSToken        dai      = new DSTokenBase(1000);
-    Feedbase       feedbase = new Feedbase();
-    FeedbaseTester tester   = new FeedbaseTester();
+contract FakeToken is ERC20
+{
+    mapping (address => uint) public   balances;
+    bool                               throwing_disabled;
 
-    uint72 id;
-
-    function setUp() {
-        tester._target(feedbase);
-        id = feedbase.claim(dai);
+    function allocate(address account, uint balance) {
+        balances[account] = balance;
     }
 
-    function test_claim() {
-        assertEq(id, uint(0));
-        assertEq(feedbase.claim(dai), uint(1));
+    function disable_throwing() {
+        throwing_disabled = true;
     }
 
-    function test_set_description() {
-        feedbase.set_description(id, "foo");
-        assertEq32("foo", feedbase.description(id));
+    function transferFrom(
+        address  account,
+        address  recipient,
+        uint     amount
+    ) returns (
+        bool     ok
+    ) {
+        if (amount <= balances[account]) {
+            balances[account]   -= amount;
+            balances[recipient] += amount;
+            ok = true;
+        } else if (throwing_disabled) {
+            ok = false;
+        } else {
+            throw;
+        }
     }
 
-    function test_read_free_feed() {
-        feedbase.publish(id, 0x42, uint40(block.timestamp + 1));
-        var (ok, value) = tester.read(id);
-        assertTrue(ok);
-        assertEq32(value, 0x42);
-    }
-
-    function testFail_set_fee_without_token() {
-        var id2 = feedbase.claim();
-        feedbase.set_fee(id2, 100);
-    }
-
-    function test_read_paid_feed() {
-        feedbase.publish(id, 0x42, uint40(block.timestamp + 1));
-        feedbase.set_fee(id, 100);
-        dai.transfer(tester, 100);
-
-        tester._target(dai);
-        ERC20(tester).approve(feedbase, 100);
-
-        tester._target(feedbase);
-        assertEq(dai.balanceOf(tester), 100);
-        var initial = dai.balanceOf(this);
-        var (ok, value) = tester.read(id);
-        assertTrue(ok);
-        assertEq32(value, 0x42);
-        assertEq(dai.balanceOf(this) - initial, 100);
-        assertEq(dai.balanceOf(tester), 0);
-    }
-
-    function test_read_paid_feed_without_payment() {
-        feedbase.publish(id, 0, uint40(block.timestamp + 1));
-        feedbase.set_fee(id, 100);
-        dai.transfer(tester, 99);
-
-        tester._target(dai);
-        DSToken(tester).approve(feedbase, 100);
-
-        tester._target(feedbase);
-        var (ok, value) = tester.read(id);
-        assertFalse(ok);
-        assertEq32(value, 0);
-    }
-
-    function test_read_paid_feed_twice() {
-        feedbase.publish(id, 0x42, uint40(block.timestamp + 1));
-        feedbase.set_fee(id, 100);
-        dai.transfer(tester, 100);
-
-        tester._target(dai);
-        ERC20(tester).approve(feedbase, 100);
-
-        tester._target(feedbase);
-
-        var pre           = dai.balanceOf(this);
-        var (ok1, value1) = tester.read(id);
-        var post1         = dai.balanceOf(this);
-        var (ok2, value2) = tester.read(id);
-        var post2         = dai.balanceOf(this);
-
-        assertTrue(ok1 && ok2);
-        assertEq(post1 - pre, 100);
-        assertEq(post2 - post1, 0);
-        assertEq32(value1, 0x42);
-        assertEq32(value2, 0x42);
-    }
-
-    function test_read_expired_feed() {
-        feedbase.publish(id, 0x42, uint40(block.timestamp - 1));
-        var (ok, value) = feedbase.read(id);
-        assertFalse(ok);
-        assertEq32(value, 0);
-    }
-
-    function test_transfer() {
-        feedbase.transfer(id, tester);
-        Feedbase(tester).publish(id, 123, uint40(block.timestamp + 1));
-        var (ok, value) = feedbase.read(id);
-        assertTrue(ok);
-        assertEq32(value, 123);
-    }
-
-    function test_events() {
-        expectEventsExact(feedbase);
-        feedbase.set_fee(id, 0);
-        Configured(id);
-        feedbase.set_description(id, "foo");
-        Configured(id);
-        feedbase.publish(id, 0x42, uint40(block.timestamp + 1));
-        Published(id);
-        feedbase.read(id);
-        Paid(id);
-        feedbase.read(id);
-        feedbase.transfer(id, tester);
-        Configured(id);
-        var id2 = feedbase.claim(dai);
-        Claimed(id2);
-    }
+    function totalSupply ()                     constant returns (uint) {}
+    function balanceOf   (address a)            constant returns (uint) {}
+    function allowance   (address a, address b) constant returns (uint) {}
+    function approve     (address a, uint x)             returns (bool) {}
+    function transfer    (address a, uint x)             returns (bool) {}
 }
